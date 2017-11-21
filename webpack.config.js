bfab4602ecc1e90d5e13b23c48a1456fd6357d11
Mandy <chomandy96@gmail.com>
2017-11-20 19:15:35 -0600

    fix configuration bugs when running handler

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,37 @@
 const slsw = require('serverless-webpack');
-var nodeExternals = require('webpack-node-externals');
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
 
 module.exports = {
-    entry: slsw.lib.entries,
-    target: 'node',
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env']
-            }
+  entry: slsw.lib.entries,
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
           }
-        },
-        {
-          test: /\.graphql$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: 'graphql-tag/loader'
-            },
-          ]
-        },
-      ]
-    },
-    output: {
-        libraryTarget: 'commonjs',
-        path: path.resolve(__dirname, '.webpack'),
-        filename: '[name].js',
-    },
-    externals: [nodeExternals()],
-  };
+        }
+      },
+      {
+        test: /\.graphql$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'graphql-tag/loader'
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.resolve(__dirname, '.webpack'),
+    filename: '[name].js'
+  },
+  externals: [nodeExternals()]
+};
