b3af58ef3463fe12043de37f237a15304e554218
Mandy <chomandy96@gmail.com>
2017-11-26 00:05:08 -0600

    complete dynamo functions

--- src/dynamo/dynamo.js
+++ src/dynamo/dynamo.js
@@ -4,29 +4,41 @@ import Promise from 'bluebird';
 const dynamo = new AWS.DynamoDB.DocumentClient();
 
 // CREATE
-export function create(args) {
+export function createItem(args) {
   return new Promise((resolve, reject) =>
     dynamo.put(args).promise()
       .then(resolve)
-      .catch(reject)
-  );
+      .catch(reject));
 }
 
 // READ
-export function get(args) {
+export function getItem(args) {
   return new Promise((resolve, reject) =>
     dynamo.get(args).promise()
       .then(resolve)
-      .catch(reject)
-  );
+      .catch(reject));
+}
+
+export function queryItems(args) {
+  return new Promise((resolve, reject) =>
+    dynamo.query(args).promise()
+      .then(resolve)
+      .catch(reject));
 }
 
 // UPDATE
+export function updateItem(args) {
+  return new Promise((resolve, reject) =>
+    dynamo.update(args).promise()
+      .then(resolve)
+      .catch(reject));
+}
 
 //
-// // DELETE
-// export function delete(args) {
-//   return new Promise((resolve, reject) =>
-//     dynamo.delete(args).promise()
-//   );
-// }
+
+export function deleteItem(args) {
+  return new Promise((resolve, reject) =>
+    dynamo.delete(args).promise()
+      .then(resolve)
+      .catch(reject));
+}
